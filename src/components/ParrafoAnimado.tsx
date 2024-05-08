7d11b85b5cc6315a3642e7d83fe69bf04ab09f0a
mendozalz <mendoza124302@gmail.com>
2024-05-08 16:18:36 -0500

    fix scroll text

--- src/components/ParrafoAnimado.tsx
+++ src/components/ParrafoAnimado.tsx
@@ -14,7 +14,7 @@ const ParrafoAnimado: React.FC<Props> = ({ parrafo }) => {
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start 0.3", "start 0.30"],
+    offset: ["start 0.4", "start 0.30"],
   });
   const palabras = parrafo!.split(" ");
 
